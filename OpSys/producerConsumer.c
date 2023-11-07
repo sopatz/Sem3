22f0b2edae1dc702da5dbdb0911f38a7aabdd79f
sopatz <sopatz04@gmail.com>
2023-11-07 15:10:24 -0500

    Update producerConsumer.c

--- OpSys/producerConsumer.c
+++ OpSys/producerConsumer.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <semaphore.h>
+#include <pthread.h> //for threads
+#include <semaphore.h> //for semaphores
 #include <unistd.h> //for sleep()
 
 #define BUFFER_SIZE 3 //so only 2 items can be on the table (2 is n - 1)
@@ -9,31 +9,31 @@
 
 //Shared Memory:
 int buffer[BUFFER_SIZE];
-int in = 0;
-int out = 0;
+int in = 0; //where in buffer the producer produces an item
+int out = 0; //where in buffer the consumer consumes an item
 
 sem_t empty, full;
 pthread_mutex_t mutex; //for mutual exclusion
 
 void* producer(void* args) {
-    int nextProduced = 0;
+    int nextProduced = 0; //producer will produce increasing integers
 
     int i;
     for (i = 0; i < TIMES_THRU; ++i) {
-        ++nextProduced;
-        sem_wait(&empty);
+        ++nextProduced; //increases number so new number is produced (for example purposes)
+        sem_wait(&empty); //decrements semaphore
         pthread_mutex_lock(&mutex); //mutex lock only contains portion of code with shared memory
 
         while(((in + 1) % BUFFER_SIZE) == out); //do nothing
-        buffer[in] = nextProduced;
-        printf("Produced %d\n", nextProduced);
-        in = (in + 1) % BUFFER_SIZE;
+        buffer[in] = nextProduced; //places the produced item into the buffer/table
+        printf("Produced %d\n", nextProduced); //prints what is produced
+        in = (in + 1) % BUFFER_SIZE; //increases in and loops to start of buffer when the end is reached
 
-        pthread_mutex_unlock(&mutex);
-        sem_post(&full);
-        sleep(2);
+        pthread_mutex_unlock(&mutex); //unlocks mutex lock
+        sem_post(&full); //increments semaphore
+        sleep(2); //wait 2ms
     }
-    pthread_exit(NULL);
+    pthread_exit(NULL); //terminates thread so it can be joined
 }
 
 void* consumer(void* args) {
@@ -41,34 +41,35 @@ void* consumer(void* args) {
 
     int i;
     for (i = 0; i < TIMES_THRU; ++i) {
-        sleep(1);
-        sem_wait(&full);
-        pthread_mutex_lock(&mutex);
+        sleep(1); //wait 1ms
+        sem_wait(&full); //decrements semaphore
+        pthread_mutex_lock(&mutex); //mutex lock only contains portion of code with shared memory
 
         while(in == out); //do nothing
-        nextConsumed = buffer[out];
+        nextConsumed = buffer[out]; //consumes item from buffer
         printf("Consumed %d\n", nextConsumed);
-        out = (out + 1) % BUFFER_SIZE;
+        out = (out + 1) % BUFFER_SIZE; //increases out and loops to start of buffer when the end is reached
 
-        pthread_mutex_unlock(&mutex);
-        sem_post(&empty);
-        --nextConsumed;
+        pthread_mutex_unlock(&mutex); //unlocks mutex lock
+        sem_post(&empty); //increments semaphore
+        --nextConsumed; //decreases number corresponding to next item consumed
     }
-    pthread_exit(NULL);
+    pthread_exit(NULL); //terminates thread so it can be joined
 }
 
 int main() {
-    pthread_t pThread, cThread;
-    sem_init(&empty, 0, BUFFER_SIZE);
-    sem_init(&full, 0, 0);
-    pthread_mutex_init(&mutex, NULL);
+    pthread_t pThread, cThread; //declaring the producer and consumer threads
+    sem_init(&empty, 0, BUFFER_SIZE); //initialize the "empty" semaphore
+    sem_init(&full, 0, 0); //initialize the "full" semaphore
+    pthread_mutex_init(&mutex, NULL); //initialize the mutual exclusion thread
 
-    pthread_create(&pThread, NULL, producer, NULL);
-    pthread_create(&cThread, NULL, consumer, NULL);
+    pthread_create(&pThread, NULL, producer, NULL); //creates new producer thread
+    pthread_create(&cThread, NULL, consumer, NULL); //creates new consumer thread
 
-    pthread_join(pThread, NULL);
-    pthread_join(cThread, NULL);
+    pthread_join(pThread, NULL); //waits for producer thread to terminate and detaches it
+    pthread_join(cThread, NULL); //waits for consumer thread to terminate and detaches it
 
+    //Object destruction:
     pthread_mutex_destroy(&mutex);
     sem_destroy(&full);
     sem_destroy(&empty);
